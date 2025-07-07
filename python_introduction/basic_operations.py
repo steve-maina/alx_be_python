e8b7b9a817bf186521fd853ce85b560ca3fa017c
Steve Maina <stevemainam@gmail.com>
2025-07-07 13:29:30 +0300

    add python_introduction/basic_operation.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number1 - number2
+multiplication = number1 * number2
+print(f"Addition of 10 and 5 is {sum}")
+print(f"Subtraction of 10 and 5 is {difference}")
+print(f"Multiplication of 10 and 5 is {multiplication}")
