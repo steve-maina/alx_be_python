be7a7735de9be626b74ee72e918178d97054845f
Steve Maina <stevemainam@gmail.com>
2025-07-21 12:35:48 +0300

    add arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,11 @@
 def perform_operation(num1, num2, operation):
     if operation == "multiply":
         return num1 * num2
-    if operation == "divide":
+    elif operation == "divide":
         if num2 == 0:
             return "Cannot divide by zero"
         return num1 / num2
-    if operation == "add":
+    elif operation == "add":
         return num1 + num2
-    if operation == "subtract":
+    elif operation == "subtract":
         return num1 - num2
