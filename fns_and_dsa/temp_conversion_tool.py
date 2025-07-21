1db85760b283d2d010faf2b64799b0b1ad1c8a7e
Steve Maina <stevemainam@gmail.com>
2025-07-21 13:28:41 +0300

    add fns_and_dsa/temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,10 +2,10 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    return FAHRENHEIT_TO_CELSIUS_FACTOR * fahrenheit
+    return FAHRENHEIT_TO_CELSIUS_FACTOR * fahrenheit -32
 
 def convert_to_fahrenheit(celsius):
-    return CELSIUS_TO_FAHRENHEIT_FACTOR * celsius
+    return CELSIUS_TO_FAHRENHEIT_FACTOR * celsius + 32
 while True:
     temp = input("Enter the temperature to convert: ")
     try:
